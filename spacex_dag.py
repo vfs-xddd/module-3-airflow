8c1439036b57ec829683bfe951c9c1e4bbdec253
vfs <62372010+vfs-xddd@users.noreply.github.com>
2021-02-26 19:19:19 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 1, 1),
+    "start_date": datetime(2005, 1, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -16,11 +16,13 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
+rokets = ['falcon1', 'falcon9', 'falconheavy']
+
+t1 = [BashOperator(
     task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+    bash_command=' '.join(["python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r", roket], 
     dag=dag
-)
+) for roket  in rokets]
 
 t2 = BashOperator(
     task_id="print_data", 
